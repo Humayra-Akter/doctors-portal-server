84c6c554e265bac9ba2db117bc797777fe5b7bae
humayra akter <humayraakter25@gmail.com>
2022-07-24 18:42:08 +0600

    first commit

--- index.js
+++ index.js
@@ -43,6 +43,35 @@ async function run() {
                         res.send(services);
                 });
 
+                app.get('/user', verifyJWT, async (req, res) => {
+                        const users = await userCollection.find().toArray();
+                        res.send(users);
+                });
+
+                app.get('/admin/:email', async (req, res) => {
+                        const email = req.params.email;
+                        const user = await userCollection.findOne({ email: email });
+                        const isAdmin = user.role === 'admin';
+                        res.send({ admin: isAdmin });
+                })
+
+                app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+                        const email = req.params.email;
+                        const requester = req.decoded.email;
+                        const requesterAccount = await userCollection.findOne({ email: requester });
+                        if (requesterAccount.role === 'admin') {
+                                const filter = { email: email };
+                                const updateDoc = {
+                                        $set: { role: admin },
+                                };
+                                const result = await userCollection.updateOne(filter, updateDoc);
+                                res.send(result);
+                        }
+                        else {
+                                res.status(403).send({ message: 'forbidden' });
+                        }
+                });
+
                 app.put('/user/:email', async (req, res) => {
                         const email = req.params.email;
                         const user = req.body;
@@ -54,7 +83,7 @@ async function run() {
                         const result = await userCollection.updateOne(filter, updateDoc, options);
                         const token = jwt.sign({ email: email }, process.env.SECRET_ACCESS_TOKEN, { expiresIn: '1h' })
                         res.send({ result, token });
-                })
+                });
 
                 app.get('/available', async (req, res) => {
                         const date = req.query.date;
